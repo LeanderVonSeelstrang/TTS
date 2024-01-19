d6cf84319ecfcad9467599de0e8490a14b53a5f2
Wiktoria Mronga <wiktoriamronga@ip-129-70-149-65.WLAN.Dyn.CIT-EC.NET>
2024-01-19 15:19:07 +0100

    Replaced code with variables

--- project.py
+++ project.py
@@ -7,15 +7,49 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 # List available TTS models
 print(TTS().list_models())
 
+base_path = '/Users/wiktoriamronga/TTS/resources/RAVDESS_Actor_02'
+
+def make_base_ref_path(gender="female", strong_intensity=False):
+    
+    return base_path + '/03-'
+    
+
+emotion_ref_dict = {
+    'neutral': ,
+    'calm': ,
+    'happy': ,
+    'sad': ,
+    'angry': ,
+    'fearful': ,
+    'disgust': ,
+    'surprised': 
+}
+
+strong_emotion_ref_dict = {
+    'neutral': ,
+    'calm': ,
+    'happy': ,
+    'sad': ,
+    'angry': ,
+    'fearful': ,
+    'disgust': ,
+    'surprised': 
+}
+
 # Initialize TTS
 #tts = TTS("tts_models/en/ljspeech/glow-tts").to(device)
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
 
+def emotional_tts(text_to_say, emotion="neutral", gender="female", strong_intensity=False, output_path="/Users/wiktoriamronga/TTS/output/ref1.wav"):
+    
+    tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+    
+    emotion_reference="/Users/wiktoriamronga/TTS/resources/ref1.wav"
+    # Run TTS
+    # Text to speech list of amplitude values as output
+    # wav = tts.tts(text=text_to_say, speaker_wav=emotion_reference, language="en")
 
+    # Text to speech to a file
+    tts.tts_to_file(text=text_to_say, speaker_wav=emotion_reference, language="en", file_path=output_path)
 
-# Run TTS
-# Text to speech list of amplitude values as output
-wav = tts.tts(text="Hello, it's Victoria, and I am working on TTS.", speaker_wav="/Users/wiktoriamronga/TTS/References/ref1.wav", language="en")
-
-# Text to speech to a file
-tts.tts_to_file(text="Hello, it's Victoria, and I am working on TTS.", speaker_wav="/Users/wiktoriamronga/TTS/References/ref1.wav", language="en", file_path="/Users/wiktoriamronga/TTS/output/ref1.wav")
+text="Hello, it's Victoria, and I am working on TTS."
+emotional_tts(text)
